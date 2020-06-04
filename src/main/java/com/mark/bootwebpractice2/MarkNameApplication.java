4b4f4fd2c4279ebc850732cc99a1e1e6b1fa817d
Ma-rk <kim.mark.dev@gmail.com>
2020-06-04 20:43:06 +0900

    Project initialized

---
+++ src/main/java/com/mark/bootwebpractice2/MarkNameApplication.java
@@ -0,0 +1,13 @@
+package com.mark.bootwebpractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mark/bootwebpractice2/MarkNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.bootwebpractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
